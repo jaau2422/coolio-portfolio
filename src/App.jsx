933171c5f7e7cf05f3bf3a4181052645c95db1d9
jaau2422 <j.aumueller@web.de>
2023-11-06 22:16:41 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+import Experience from './Experience.jsx'
+
+function App() {
+
+  return (
+// tailwind css usage
+   
+     
+   <Experience />
+      
+ 
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {Canvas} from "@react-three/fiber";
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 2000,
+            position: [ -3, 1.5, 4 ]
+        } }
+    >
+        <App />
+    </Canvas>
+)
